45f67469e0ce2c4785ca722b085ed29af030e2a9
pritamsap <pritamsap2@gmail.com>
2023-12-27 23:06:50 -0500

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by pritam on 2023-12-27.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
